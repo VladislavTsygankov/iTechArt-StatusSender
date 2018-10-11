11ffa45dae6cb8bd46532838a84831212d726e19
VladislavTsygankov <tsygan-97@mail.ru>
2018-10-11 17:00:17 +0300

    folder - config

--- src/db/db-connection.js
+++ src/db/db-connection.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import databaseConfig from './db-config';
+import databaseConfig from './config/db-config';
 
 const sequelize = new Sequelize(
   databaseConfig.DATABASE,
